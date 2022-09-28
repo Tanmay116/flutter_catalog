7fee668efaa18832557b1ddb4587f8d9b40864c7
Tanmay Mistry <tanmay.mistry123@gmail.com>
2022-09-29 01:12:08 +0530

    day10_completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,22 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+}
+
+final products = [
+  Item(
+    id: "Item1",
+    name: "Iphone 12 Pro",
+    desc: "Aplle",
+    price: 999,
+    color: "3350a",
+    image:
+        "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MHKK3?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1603649004000",
+  )
+];
